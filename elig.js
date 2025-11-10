4024c36e777e51e8143be140e23fc94bb09f34e8
Ray James Gonzales <54382521+EvacionSaraak@users.noreply.github.com>
2025-11-10 15:19:15 +0400

    Update elig.js

--- elig.js
+++ elig.js
@@ -180,6 +180,7 @@ function prepareEligibilityMap(eligArray) {
         if (!memberID) return;
         eligMap.set(memberID, normalizedRecord);
     });
+    console.log(eligMap);
     return eligMap;
 }
 
